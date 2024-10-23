d4d1f94ba038d156b882e4f2a258085603e93f85
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-10-23 16:14:39 +0200

    first version of React support

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src="https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg" alt="Logo" />
+        <nav>
+          <a href="/">Home</a>
+          <a href="/contact"></a>
+        </nav>
+      </header>
+
+      <figure>
+        <img src="https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/10-intelligent-dog-breeds/golden-retriever-tongue-out.jpg" alt="Golden Retriever" />
+        <figcaption>Dogs</figcaption>
+      </figure>
+      <figure>
+        <img src="https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg" alt="A cat" />
+        <figcaption>Cats</figcaption>
+      </figure>
+    </>
+  )
+}
+
+export default App
